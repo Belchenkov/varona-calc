c26ceb8870885d626d6ee1da61b5a1159d4f73bc
Belchenkov <u608110@gmail.com>
2017-11-01 13:19:45 +0300

    Section Buttons

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass        = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['src/scss/*.scss'])
         .pipe(sass())
+        .pipe(autoprefixer())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
@@ -24,6 +26,7 @@ gulp.task('serve', ['sass'], function() {
         server: "./src"  
     });
 
+    gulp.watch(['src/scss/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
