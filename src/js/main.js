0990a764227821760709de1a94882e86026ae7d7
Belchenkov <u608110@gmail.com>
2017-11-05 13:50:48 +0300

    Header is ready

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,20 @@ $(document).ready(function(){
    
 
 });     
+
+new Vue({
+    el: '#calc',
+    data: {
+        amountComp: 5,
+        amountServer: 5,
+        basePlan: false
+    },
+    methods: {
+        rangeComp: function (event) {
+            this.total = event.target.value;
+        },
+        rangeServer: function (event) {
+            this.total = event.target.value;
+        }
+    }
+});
